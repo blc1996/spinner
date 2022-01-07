2c29765e6cf856e0b13f01ead48384cc882f1037
Lucheng Bao <luchengb@vmware.com>
2022-01-07 12:29:23 -0800

    test1

--- spinner.go
+++ spinner.go
@@ -339,7 +339,6 @@ func (s *Spinner) Start() {
 // Stop stops the indicator.
 func (s *Spinner) Stop() {
 	s.mu.Lock()
-	defer fmt.Print("\n")
 	defer s.mu.Unlock()
 	if s.active {
 		s.active = false
